c6c4d2c507f07173c529dd40c66a8af0c2e8097f
vitorbite <vitorbitencourt20@gmail.com>
2026-05-23 22:20:42 -0300

    Criando a classe Node

---
+++ src/main/java/Node.java
@@ -0,0 +1,9 @@
+public class Node {
+    protected String value;
+    protected Node left;
+    protected Node right;
+     Node(String value){
+        this.value = value;
+        left = right = null;
+    }
+}
