a2a57e2170059dc14b6668afc6dc7df4168b5ad4
vitorbite <vitorbitencourt20@gmail.com>
2026-05-28 00:38:48 -0300

    Implementando o método de recoloração

--- src/main/java/RB_Router_Tree.java
+++ src/main/java/RB_Router_Tree.java
@@ -1,70 +1,92 @@
 public class RB_Router_Tree extends Binary_Search_Tree<RB_Node> {
 
-    //
+    public void RB_Insert_Fixup(RB_Node node) {
+    // Método de Recoloração
+    while (node.parent != null && node.parent.isRed()) {
+        if (node.parent == node.parent.parent.left) {
+            RB_Node uncle = node.parent.parent.right;
 
-    public RB_Node RB_Insert_Fixup(RB_Node node) {
-        return new RB_Node(null, root, root, root);
-    }
-
-    @Override
-    public RB_Node insert(RB_Node node, PacketRule rule) {
-
-        RB_Node newNode = new RB_Node(rule, null, null, null);
-        RB_Node leaf = null;
-        RB_Node root = this.root; 
-
-        while (root != null) {
-            leaf = root;
-            if (newNode.rule.getId() < root.rule.getId()) {
-                root = root.left;
-            } else if (newNode.rule.getId() > root.rule.getId()) {
-                root = root.right;
+            if (uncle != null && uncle.isRed()) {
+                node.parent.makeBlack();
+                uncle.makeBlack();
+                node.parent.parent.makeRed();
+                node = node.parent.parent;
             } else {
-                return root; 
+                if (node == node.parent.right) {
+                    node = node.parent;
+                    Left_Rotation(this.root, node);
+                }
+                node.parent.makeBlack();
+                node.parent.parent.makeRed();
+                Right_Rotation(this.root, node.parent.parent);
+            }
+        } else {
+            RB_Node uncle = node.parent.parent.left;
+            
+            if (uncle != null && uncle.isRed()) {
+                node.parent.makeBlack();
+                uncle.makeBlack();
+                node.parent.parent.makeRed();
+                node = node.parent.parent;
+            } else {
+                if (node == node.parent.left) {
+                    node = node.parent;
+                    Right_Rotation(this.root, node);
+                }
+                node.parent.makeBlack();
+                node.parent.parent.makeRed();
+                Left_Rotation(this.root, node.parent.parent);
             }
         }
-
-        newNode.parent = leaf;
-
-        if (leaf == null) {
-            this.root = newNode;
-        } else if (newNode.rule.getId() < leaf.rule.getId()) {
-            leaf.left = newNode;
-        } else {
-            leaf.right = newNode;
-        }
-
-        newNode.left = null;
-        newNode.right = null;
-        newNode.makeRed(); 
-
-        RB_Insert_Fixup(newNode);
-
-        return newNode;
     }
+    this.root.makeBlack();
+}
+
+@Override
+public RB_Node insert(RB_Node node, PacketRule rule) {
+    RB_Node newNode = new RB_Node(rule, null, null, null);
+    RB_Node leaf = null;
+    RB_Node currentRoot = this.root; 
+
+    while (currentRoot != null) {
+        leaf = currentRoot;
+        if (newNode.rule.getId() < currentRoot.rule.getId()) {
+            currentRoot = currentRoot.left;
+        } else if (newNode.rule.getId() > currentRoot.rule.getId()) {
+            currentRoot = currentRoot.right;
+        } else {
+            return currentRoot;
+        }
+    }
+
+    newNode.parent = leaf;
+
+    if (leaf == null) {
+        this.root = newNode;
+    } else if (newNode.rule.getId() < leaf.rule.getId()) {
+        leaf.left = newNode;
+    } else {
+        leaf.right = newNode;
+    }
+
+    newNode.left = null;
+    newNode.right = null;
+    newNode.makeRed();
+
+    RB_Insert_Fixup(newNode);
+    return newNode;
+}
 
     @Override
     public RB_Node delete(RB_Node node, PacketRule rule) {
         return null;
     }
 
-    public void remedleafDoubleRed(RB_Node node) {
+    public void Left_Rotation(RB_Node root, RB_Node node) {
 
     }
 
-    public void Single_Left_Rotation(RB_Node node) {
-
-    }
-
-    public void Single_Right_Rotation(RB_Node node) {
-
-    }
-
-    public void Double_Left_Right_Rotation(RB_Node node) {
-
-    }
-
-    public void Double_Right_Left_Rotation(RB_Node node) {
+    public void Right_Rotation(RB_Node root, RB_Node node) {
 
     }
 
